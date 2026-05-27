a7fd019200af829fd08529ba6dd8a6fbf1e6edf9
vkhoma <16196348+HVV83@users.noreply.github.com>
2026-05-27 20:15:32 +0200

    feat(config): add Spring Cloud Config client integration and retry configuration

--- src/test/java/com/polarbookshop/orderservice/OrderServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/orderservice/OrderServiceApplicationTests.java
@@ -8,6 +8,7 @@ import com.polarbookshop.orderservice.order.web.OrderRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webtestclient.autoconfigure.AutoConfigureWebTestClient;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -23,6 +24,7 @@ import static org.mockito.BDDMockito.given;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
+@AutoConfigureWebTestClient
 class OrderServiceApplicationTests {
 
     @Container
