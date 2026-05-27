b490f8d214d2ccd865d717bd4a20609358c83f80
vkhoma <16196348+HVV83@users.noreply.github.com>
2026-05-27 12:13:44 +0200

    feat(order-service): integrate catalog-service WebClient and book lookup in OrderService

---
+++ src/main/java/com/polarbookshop/orderservice/config/ClientConfig.java
@@ -0,0 +1,26 @@
+package com.polarbookshop.orderservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class ClientConfig {
+
+    @Bean
+    WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
+
+
+    @Bean
+    WebClient webClient(
+            ClientProperties clientProperties,
+            WebClient.Builder webclientBuilder
+    ) {
+        return webclientBuilder
+                .baseUrl(clientProperties.catalogServiceUri().toString())
+                .build();
+    }
+
+}
