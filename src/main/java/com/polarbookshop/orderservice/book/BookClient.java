f72bcbbf6c5870fb9576bfc4f28c17c47f52429b
vkhoma <16196348+HVV83@users.noreply.github.com>
2026-05-27 17:48:59 +0200

    feat(order): add OrderController, OrderService and OrderRequest with validation

--- src/main/java/com/polarbookshop/orderservice/book/BookClient.java
+++ src/main/java/com/polarbookshop/orderservice/book/BookClient.java
@@ -2,7 +2,11 @@ package com.polarbookshop.orderservice.book;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
+
+import java.time.Duration;
 
 @Component
 public class BookClient {
@@ -17,7 +21,13 @@ public class BookClient {
                 .get()
                 .uri(BOOKS_ROOT_API + isbn)
                 .retrieve()
-                .bodyToMono(Book.class);
+                .bodyToMono(Book.class)
+                .timeout(Duration.ofSeconds(3), Mono.empty())
+                // Returns an empty object when a 404 response is received
+                .onErrorResume(WebClientResponseException.NotFound.class,exception -> Mono.empty())
+                .retryWhen(Retry.backoff(3, Duration.ofMillis(100)))
+                // If any error happens after the 3 retry attempts, catch the exception and return an empty object.
+                .onErrorResume(Exception.class, exception -> Mono.empty());
     }
 
 }
